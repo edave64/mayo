e457649ab8ec41393d61276f02f4cffa6a2363cf
edave64 <github5815@xcvbnm.org>
2025-11-01 23:12:52 +0100

    Improve "plane" model slightly

--- camera_3d.gd
+++ camera_3d.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var plane = get_node("../Plane")
 	position = plane.get_position()
-	position.z += 2
-	position.y += 3
+	position.z += 8
+	position.y += 4
 	rotation.x = -0.3
 	pass

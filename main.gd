0231f84edb39f0353e9ec59e1047a466e7474a1a
edave64 <github5815@xcvbnm.org>
2025-11-14 22:52:16 +0100

    Day 14: Improved world gen

--- main.gd
+++ main.gd
@@ -12,6 +12,6 @@ func _process(delta: float) -> void:
 		Input.get_axis("vehicle_left", "vehicle_right"),
 		Input.get_axis("plane_break", "plane_throttle") * 50,
 		Input.get_axis("vehicle_up", "vehicle_down"),
-	)  * delta * 2000
+	)  * delta * 20000
 
 	$Camera3D.tracking.apply_force(force)
--- terrain_generator.gd
+++ terrain_generator.gd
@@ -3,15 +3,40 @@ extends Node3D
 
 class_name TerrainGenerator
 
+# Based on a YT tutorial by DevPoodle
+# https://www.youtube.com/watch?v=6qim01M1Yp0
+
+# Generates a world in chunks based off a given noise, with mesh and collision.
+# The world is generated in chunks and tracks a given Node3D object. If the
+# object moves towards unloaded chunks, they will be loaded automatically.
+# 
+# Termininology:
+# - Chunk: A square piece of the world, with the size `size`
+# - Chunk mesh: A child node of this generator. It's expected to be a
+#   `MeshInstance3D` with a `StaticBody3D` with a `CollisionShape3D` child.
+# - Chunk mesh idx: The chunk meshes are stored in a flat array. This is the
+#   index of the mesh in that array.
+# - X and Y: An absolute position in the world
+# - Grid X and Grid Y: The integer position of the chunk in the world grid.
+#   Essentially, the integer division of the absolute position by `size`
+#
+# Chunks are generated as needed by a thread pool. The main thread is blocked
+# only if the chunk the tracked object is currently in is not yet loaded.
+
 const size := 256.0
 
+# The object that is tracked by the generator. The world will be generated
+# around this object, and if it moves, the chunks will be loaded automatically.
 @export var tracking: Node3D
 
+# The resolution of the world. Determines the amount of vertices in each chunk
+# mesh and the precision of the height map.
 @export_range(4, 256, 4) var resolution := 32:
 	set(new_resolution):
 		resolution = new_resolution
 		invalidate_all_chunks()
 
+# The noise generator used to generate the height map.
 @export var noise: FastNoiseLite:
 	set(new_noise):
 		if noise:
@@ -21,43 +46,31 @@ const size := 256.0
 		if noise:
 			noise.changed.connect(invalidate_all_chunks)
 
+# The maximum height of the world.
 @export_range(4, 128, 4) var height = 64:
 	set(new_height):
 		height = new_height
 		invalidate_all_chunks()
 
-var thread_pool: Array[Thread] = [
-	Thread.new(),
-	Thread.new(),
-	Thread.new(),
-]
-
-var grid_children
-
 func _ready() -> void:
-	grid_children = [
-		[
-			get_node('TerrainTL'),
-			get_node('TerrainCL'),
-			get_node('TerrainBL')
-		],
-		[
-			get_node('TerrainTC'),
-			get_node('TerrainCC'),
-			get_node('TerrainBC')
-		],
-		[
-			get_node('TerrainTR'),
-			get_node('TerrainCR'),
-			get_node('TerrainBR')
-		]
+	chunk_meshes = [
+		get_node('TerrainTL'),
+		get_node('TerrainTC'),
+		get_node('TerrainTR'),
+		get_node('TerrainCL'),
+		get_node('TerrainCC'),
+		get_node('TerrainCR'),
+		get_node('TerrainBL'),
+		get_node('TerrainBC'),
+		get_node('TerrainBR')
 	]
+	
+	task_by_chunk_mesh.resize(chunk_meshes.size())
 	
 	for thread in thread_pool:
 		thread.start(worker)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if not tracking: return
 	
 	var current_x = int(round(tracking.position.x / size))
@@ -76,70 +89,115 @@ func _process(delta: float) -> void:
 			queue_chunk_load_if_needed(current_x + x, current_y + y)
 	
 	# Step 3: IF the current chunk is not yet finished, block until it is
-	
 	block_until_loaded(current_x, current_y)
 
-var loaded_chunks: Array[String] = []
+# The thread pool used to generate the chunks
+var thread_pool: Array[Thread] = [
+	Thread.new(),
+	Thread.new(),
+	Thread.new(),
+]
 
-func is_chunk_loaded(x: int, y: int) -> bool:
+# Since what we can render is limited by the amount of chunk meshes, we only
+# need to track up to one task per chunk mesh. So this array needs to be the
+# same size as the number of chunk meshes.
+var task_by_chunk_mesh: Array[WorldGenTask] = [null,null,null,null,null,null,null,null,null]
+
+var chunk_meshes: Array[MeshInstance3D]
+
+# The number of available chunk meshes for the x and y directions
+const chunk_meshes_x = 3
+const chunk_meshes_y = 3
+
+# Every chunk position maps to one chunk mesh that will render it.
+# And since godot doesn't like nested collections, we address each of those
+# with a single number
+func get_chunk_mesh_idx(x: int, y: int) -> int:
+	var g_x = imod(x + 1, chunk_meshes_x)
+	var g_y = imod(y + 1, chunk_meshes_x)
+	return g_y * chunk_meshes_y + g_x
+
+# Returns true if a given chunk is already generated or being generated
+func is_chunk_active(x: int, y: int) -> bool:
 	# Ensure only the main thread interacts with loaded_chunks, so we don't need
 	# mutexes
 	assert(OS.get_thread_caller_id() == OS.get_main_thread_id())
-	return loaded_chunks.has(str(x, '_', y))
-
-func set_chunk_loaded(x: int, y: int) -> void:
-	assert(OS.get_thread_caller_id() == OS.get_main_thread_id())
-	loaded_chunks.push_back(str(x, '_', y))
+	var chunk_mesh_idx = get_chunk_mesh_idx(x, y)
+	var task = task_by_chunk_mesh[chunk_mesh_idx]
+	return task && task.grid_x == x && task.grid_y == y
 
 func invalidate_all_chunks() -> void:
 	assert(OS.get_thread_caller_id() == OS.get_main_thread_id())
-	loaded_chunks = []
+	task_by_chunk_mesh = [null,null,null,null,null,null,null,null,null]
 
-func get_child_for_chunk(x: int, y: int) -> MeshInstance3D:
-	var x_slice = grid_children[imod(x + 1, grid_children.size())];
-	return x_slice[imod(y + 1, x_slice.size())]
-
+# True integer modulo
 func imod(a: int, b: int) -> int:
 	return (a % b + b) % b
 
 func queue_chunk_load_if_needed(x: int, y: int) -> void:
-	if is_chunk_loaded(x, y): return
-	set_chunk_loaded(x, y)
+	if is_chunk_active(x, y): return
 	
 	task_queue_mutex.lock()
 	var task = WorldGenTask.new(self, x, y)
+	task_by_chunk_mesh[get_chunk_mesh_idx(x, y)] = task
 	task_queue.push_back(task)
 	task_queue_mutex.unlock()
 	task_queue_semaphore.post()
 
 func block_until_loaded(x: int, y: int) -> void:
-	if is_chunk_loaded(x, y): return
-	set_chunk_loaded(x, y)
+	assert(OS.get_thread_caller_id() == OS.get_main_thread_id())
+	var chunk_mesh_idx = get_chunk_mesh_idx(x, y)
+	var task = task_by_chunk_mesh[chunk_mesh_idx]
 	
-	var task = WorldGenTask.new(self, x, y)
+	if task && task.grid_x == x && task.grid_y == y && task.realized:
+		# Happy case, chunk is ready
+		return
+	
+	# Chunk hasn't even started loading
+	# -> Just load it on the main thread at this point
+	task = WorldGenTask.new(self, x, y)
+	task_by_chunk_mesh[get_chunk_mesh_idx(x, y)] = task
 	task.work_in_thread()
 	task.realize_in_main()
 
+	# It's possible that the chunk we are generating here was already being
+	# generated by another thread. But trying to block this thread until some
+	# other thread finishes seemed more effort than it's worth. Instead, the
+	# other thread will just realize at the end that its place in the
+	# task_by_chunk_mesh array is taken.
+
+# The list of finished tasks
+# Has a mutex, since the worker threads can push onto it and the main thread
+# can pop from it
 var finished_tasks = []
 var finished_tasks_mutex = Mutex.new()
+
+# The queue of tasks that need to be worked on
+# Has a mutex, since the main thread can push onto it and the worker threads
+# can pop from it
+# Also has a semaphore, which is used to wake the worker threads when there
+# are new tasks to be worked on
 var task_queue = []
 var task_queue_mutex = Mutex.new();
 var task_queue_semaphore = Semaphore.new()
 
 class WorldGenTask:
 	var completed = false
+	var realized = false
 	var grid_x: int
 	var grid_y: int
 	var parent: TerrainGenerator
-	
-	var height_map_data: PackedFloat32Array
-	var array_mesh: ArrayMesh
+
+	# Values copied from the parent
 	var noise: FastNoiseLite
 	var height: float
 	var resolution: int
+
+	# Results
+	var array_mesh: ArrayMesh
+	var height_map_data: PackedFloat32Array
 	
 	func _init(parent_: TerrainGenerator, x: int, y: int) -> void:
-		print("init ", x, ' ', y)
 		grid_x = x
 		grid_y = y
 		parent = parent_
@@ -148,11 +206,9 @@ class WorldGenTask:
 		resolution = parent.resolution
 	
 	func work_in_thread() -> void:
-		print("starting ", grid_x, ' ', grid_y)
 		generate_mesh()
 		generate_height_map()
 		completed = true
-		print("finished ", grid_x, ' ', grid_y)
 	
 	func generate_mesh() -> void:
 		var plane = PlaneMesh.new()
@@ -190,8 +246,6 @@ class WorldGenTask:
 	func generate_height_map():
 		var height_map_scale: float = size / resolution
 		
-		# We don't want to scale y, but the collision shape wants a uniform scale
-		
 		var offset_x = grid_x * size
 		var offset_y = grid_y * size
 		
@@ -200,10 +254,12 @@ class WorldGenTask:
 		var data = PackedFloat32Array()
 		data.resize(map_resolution * map_resolution)
 		
+		@warning_ignore("integer_division")
 		var half_res = (map_resolution) / 2
 		
 		for i:int in data.size():
 			var x = (i % map_resolution - half_res) * height_map_scale
+			@warning_ignore("integer_division")
 			var y = (i / map_resolution - half_res) * height_map_scale
 			
 			data[i] = get_height(offset_x + x, offset_y + y) / (height_map_scale)
@@ -214,9 +270,13 @@ class WorldGenTask:
 		# Editing the node tree should only be done in the main thread
 		assert(OS.get_thread_caller_id() == OS.get_main_thread_id())
 		assert(completed)
-		print("realizing ", grid_x, ' ', grid_y)
 		
-		var child = parent.get_child_for_chunk(grid_x, grid_y)
+		var idx = parent.get_chunk_mesh_idx(grid_x, grid_y)
+		
+		# The task has been replaced in the mean-time
+		if parent.task_by_chunk_mesh[idx] != self: return;
+		
+		var child = parent.chunk_meshes[idx]
 		var collision: CollisionShape3D = child.get_node('StaticBody3D/Collision')
 		
 		var offset_x = grid_x * size
@@ -233,9 +293,7 @@ class WorldGenTask:
 		
 		var height_map_scale: float = size / resolution
 		
-		collision.scale.x = height_map_scale
-		collision.scale.y = height_map_scale
-		collision.scale.z = height_map_scale
+		collision.scale = Vector3.ONE * height_map_scale
 		
 		var height_map_shape: HeightMapShape3D = collision.shape
 		
@@ -243,7 +301,7 @@ class WorldGenTask:
 		height_map_shape.map_width = map_resolution
 		height_map_shape.map_data = height_map_data
 		
-		print("loaded ", grid_x, ' ', grid_y)
+		realized = true
 	
 	func get_height(x: float, y: float) -> float:
 		return noise.get_noise_2d(x, y) * height
